7413dee23d7c97ec4cfa357581e5dec3b6268355
Lounes25CCT <lounes.ismal@gmail.com>
2025-11-25 10:18:15 +0000

    First commit - no code

--- src/myfirstgithub/MyFirstGitHub.java
+++ src/myfirstgithub/MyFirstGitHub.java
@@ -15,6 +15,8 @@ public class MyFirstGitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello Guys");
     }
     
 }
